59878c3f1650307e7b7b8497c6545dbded89f7f2
Nazaf64 <aritroster@gmail.com>
2026-09-08 14:07:04 +0600

    Beginning of task 04. Moved data to Board from ListBoard.

--- 03 Thinking-in-React/tapaScript-cars/src/ListBoard.jsx
+++ 03 Thinking-in-React/tapaScript-cars/src/ListBoard.jsx
@@ -1,13 +1,13 @@
 import Car from './Car';
 
 function ListBoard() {
-    const cars = [   
-  { id: 1, title: "Luxury Sedan", brand: "Mercedes", year: 2023, price: 80000, isPremium: true },
-  { id: 2, title: "Family SUV", brand: "Toyota", year: 2022, price: 45000, isPremium: false },
-  { id: 3, title: "Sports Car", brand: "Porsche", year: 2023, price: 120000, isPremium: true },
-  { id: 4, title: "Electric Hatchback", brand: "Nissan", year: 2022, price: 35000, isPremium: false },
-  { id: 5, title: "Luxury SUV", brand: "BMW", year: 2023, price: 90000, isPremium: true },
-];
+//     const cars = [   
+//   { id: 1, title: "Luxury Sedan", brand: "Mercedes", year: 2023, price: 80000, isPremium: true },
+//   { id: 2, title: "Family SUV", brand: "Toyota", year: 2022, price: 45000, isPremium: false },
+//   { id: 3, title: "Sports Car", brand: "Porsche", year: 2023, price: 120000, isPremium: true },
+//   { id: 4, title: "Electric Hatchback", brand: "Nissan", year: 2022, price: 35000, isPremium: false },
+//   { id: 5, title: "Luxury SUV", brand: "BMW", year: 2023, price: 90000, isPremium: true },
+// ];
 
     return (
         <ul className='flex flex-wrap'>
